6456f7790ce103ff113b8cafcde4af86d1cfceff
Prabal-saxena <prabalsaxena98@gmail.com>
2025-06-19 23:43:53 -0400

    Update Product API Grant CORS

--- src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.ecommerce.product_service.repository;
 import com.ecommerce.product_service.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends MongoRepository<Product,String> {
+
+    Optional<Product> findByName(String name);
 }
--- src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +22,7 @@ public class ProductService {
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
+                .imageUrl(productRequest.getImageUrl())
                 .build();
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
@@ -31,12 +33,40 @@ public class ProductService {
         return products.stream().map(product -> mapToProductResponse(product)).toList();
     }
 
+    public Optional<Product> updateProduct(ProductRequest productRequest){
+
+        if(productRequest == null || productRequest.getName() == null || productRequest.getName().trim().isEmpty()){
+            System.err.println("Error: ProductRequest or product name for update cannot be null or empty.");
+            return Optional.empty();
+        }
+
+        Optional<Product> existingProduct= productRepository.findByName(productRequest.getName());
+        if(existingProduct.isPresent()) {
+            Product product = existingProduct.get();
+            System.out.println("Found product " + productRequest.getName() + " now updating details...");
+
+            product.setDescription(productRequest.getDescription());
+            product.setPrice(productRequest.getPrice());
+            product.setImageUrl(productRequest.getImageUrl());
+
+            Product updatedProduct = productRepository.save(product);
+            System.out.println("Product '" + productRequest.getName() + "' updated successfully.");
+            return Optional.of(updatedProduct);
+        }else {
+            // Product not found
+            System.out.println("Product with name '" + productRequest.getName() + "' not found for update.");
+            return Optional.empty(); // Indicate that the product was not found
+        }
+
+    }
+
     private ProductResponse mapToProductResponse(Product product) {
         return ProductResponse.builder()
                 .id(product.getId())
                 .name(product.getName())
                 .description(product.getDescription())
                 .price(product.getPrice())
+                .imageUrl(product.getImageUrl())
                 .build();
     }
 
